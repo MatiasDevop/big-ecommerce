556a4fe77ccb803d89e7cbc77677694cc4a7699e
Matt <solomatias321@gmail.com>
2024-03-05 18:41:22 +0100

    set up products comp

--- src/app/pages/admin/products/products.component.ts
+++ src/app/pages/admin/products/products.component.ts
@@ -1,12 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
 
+  isSidePanelVisible: boolean = true;
+
+  productObj: any = {
+    "ProductId": 0,
+    "ProductSku": "",
+    "ProductName": "",
+    "ProductPrice": 0,
+    "ProductShortName": "",
+    "ProductDescription": "",
+    "CreatedDate": new Date(),
+    "DeliveryTimeSpan": "",
+    "CategoryId": 0,
+    "ProductImageUrl": ""
+  }
+
+  openSidePanel(){
+    this.isSidePanelVisible = true;
+  }
+
+  closeSidePanel() {
+    this.isSidePanelVisible = false;
+  }
 }
